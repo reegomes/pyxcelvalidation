3af92a063ffddbda06b31c82ecb38c14a6f5957f
Renato Gomes <reegomescel@gmail.com>
2018-03-08 17:23:45 -0300

    Add files via upload

---
+++ procv.py
@@ -0,0 +1,40 @@
+from openpyxl import load_workbook
+wb = load_workbook(filename='teste.xlsx', read_only=False)
+ws = wb['Sheet']
+
+# Contador de linha e delimitador de linhas da planilha
+line = 1
+lines = 10
+
+# Talvez essas linhas aqui estão sendo inuteis, só talvez
+check1 = ws.iter_cols(min_row=linha, min_col=2, max_col=2, max_row=linha)
+check2 = ws.iter_cols(min_row=linha, min_col=8, max_col=8, max_row=linha)
+nnfe = ws.iter_cols(min_row=linha, min_col=7, max_col=7, max_row=linha)
+
+# Listas
+nomes1 = []
+nomes2 = []
+linhanfe = []
+
+
+for col in ws.iter_cols(min_row=1, min_col=8, max_col=8, max_row=lines):
+    for cell in col:
+        linha += 1
+        check1 = cell.value
+        
+for col in ws.iter_cols(min_row=1, min_col=2, max_col=2, max_row=lines):
+    for cell in col:
+        check2 = cell.value
+        
+for col in ws.iter_cols(min_row=1, min_col=7, max_col=7, max_row=lines):
+    for cell in col:
+        nfe = ws.iter_cols(min_row=line, min_col=7, max_col=7, max_row=line)
+        nnfe = (cell.value)
+
+if (check1 == check2):
+    print('Valores iguais para', check1,' numero da nota', nnfe,'.')
+else:
+    print("Diferentes")
+
+#for n in nomes:
+#    print(n)
